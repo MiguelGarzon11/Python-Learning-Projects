59741c00ae6e42ca90871f687d37a9408d5c6916
Miguel Angel <miguelangelgarb@gmail.com>
2025-03-31 23:28:33 -0500

    Ejercicios de Builtin functions con Modulos

--- Modulos/builtin_functions.py
+++ Modulos/builtin_functions.py
@@ -2,6 +2,38 @@
 
 from functools import reduce
 
+# 1. Convertir temperaturas de Celsius a Fahrenheit usando map()
+
 def celsius_a_fahrenheit(temp):
     return (temp * 9/5) + 32
 
+def convertir_temperaturas(lista_temperaturas):
+    return list(map(celsius_a_fahrenheit, lista_temperaturas ))
+
+# 2. Filtar números pares usando filter()
+
+def es_par(num):
+    return num % 2 == 0
+
+def obtener_pares(lista_numeros):
+    return list(filter(es_par, lista_numeros))
+
+# 3. Calcular el producto de una lista usando reduce()
+
+def producto_lista(lista_numeros):
+    return reduce(lambda x,y: x * y, lista_numeros)
+
+# lambda es lo mismo que si hicieramos esto:
+# def multiplicar(x, y):
+#   return x * y 
+# reduce aplica esta función acumulativamente a todos los elementos de la lista
+
+# 4. Combinar dos listas en una lista de tuplas zip()
+
+def combinar_listas(lista1, lista2):
+    return list(zip(lista1, lista2)) # zip() toma elementos de ambas listas y los empareja en tuplas
+
+# 5. Ordenar una lista de palabras por su longitud usando sorted()
+
+def ordenar_por_longitud(lista_palabras):
+    return sorted(lista_palabras, key=len) # sorted() ordena la lista usando len como clave de ordenación
--- Modulos/test_builtin.py
+++ Modulos/test_builtin.py
@@ -1,4 +1,27 @@
+# Test de funciones builtin
 
+import builtin_functions as bf # Utilizo as para renombrar el modulo y utilizar un nombre más corto
+
+# Conversión de temperaturas
+temperaturas = [0, 25, 100]
+print("Temperaturas convertidas:", bf.convertir_temperaturas(temperaturas))
+
+# Función que filtra los números pares
+numeros = [1,2,3,4,5,6,7,8]
+print("Números pares:", bf.obtener_pares(numeros))
+
+# Producto de una lista de números
+numeros_producto = [1,2,3,4,5]
+print("Producto de la lista:", bf.producto_lista(numeros_producto))
+
+# Combinar dos listas
+lista1 = ['a','b','c']
+lista2 = [1,2,3]
+print("Listas combinadas:", bf.combinar_listas(lista1, lista2))
+
+# Ordenar palabras por longitud
+palabras = ["Python", "Java", "C++", "Css"]
+print("Palabras ordenadas:", bf.ordenar_por_longitud(palabras))
 
 
 

89a9a780603c0ed8bcc3f9ae9dfe650f61413ab8
Miguel Angel <miguelangelgarb@gmail.com>
2025-04-02 18:11:46 -0500

    Regular Expresions

---
+++ Regular Expresions/RegularExpresions.py
@@ -0,0 +1,36 @@
+# Ejercicios para entender Regular Expresions (Regex).
+
+# . → Representa cualquier carácter
+
+# \d → Representa un dígito (0-9)
+
+# \w → Representa un carácter alfanumérico (letras y números)
+
+# \s → Representa un espacio en blanco
+
+# * → Cero o más repeticiones del carácter anterior
+
+# + → Una o más repeticiones
+
+# ? → Cero o una repetición
+
+# {n} → Exactamente n repeticiones
+
+# [] → Agrupa caracteres específicos ([aeiou] busca vocales)
+
+# ^ → Indica inicio de una línea
+
+# $ → Indica fin de una línea
+
+import re
+
+# texto = "Cumplimos 624 días juntos con mi pollita y Mi novia no quiere venir a verme y me\n " \
+# "siento muy triste porque la extraño mucho y me hace muchisima falta"
+# patron = r"\d+" # Encuentra todos los números
+# resultado = re.findall(patron, texto)
+# print(resultado, texto)
+
+texto = "Hola estoy estudiando Regular Expresions de Python"
+patron = r"\b[A-Z]\w*"
+resultado = re.findall(patron, texto)
+print(resultado)

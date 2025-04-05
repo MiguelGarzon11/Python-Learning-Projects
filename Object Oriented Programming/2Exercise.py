69ef7dbe5ac00b5a6d1ee38920b02a402cf8aaca
Miguel Angel <miguelangelgarb@gmail.com>
2025-04-05 12:47:42 -0500

    Ejercicio de Cajero OOP

--- Object Oriented Programming/2Exercise.py
+++ Object Oriented Programming/2Exercise.py
@@ -29,14 +29,53 @@ class Cajero:
 
         print("Entregando:")
         for billete, cantidad in contador.items():
-            print(f"{cantidad} billete(s) de $ {billete:,.0f}".replace(".", ","))
+            print(f"{cantidad} billete(s) de $ {billete:,.0f}".replace(".",","))
 
-        print(f"Saldo restante en el cajero: $ {self.saldo:,.0f}".replace(".", ","))
+        print(f"Saldo restante en el cajero: $ {self.saldo:,.0f}".replace(".",","))
+
+    def depositar(self, monto):
+        if monto < 5000:
+            print("\nError: Deposite más de 5.000\n")
+        else:
+            self.saldo = self.saldo + monto
+            print(f"\n¡ Se deposito exitosamente ${monto:,.0f}!".replace(".",","))
+            return self.saldo
+        
+    def mostrar_saldo(self):
+        if self.saldo == 0 or self.saldo <= 5000:
+            print("\nEl cajero no tiene suficiente dinero.")
+        else:
+            print(f"El saldo del cajero es de: $ {self.saldo:,.0f} pesos".replace(".",","))
 
 cajero = Cajero(200000)
-cajero.retirar(155000)
-cajero.retirar(10000)
-cajero.retirar(85000)
 
-            
-            
+while True:
+    try:
+        print("\nC A J E R O  A U T O M Á T I C O")
+        print("  M E N Ú  P R I N C I P A L \n")
+        print("1. Retirar.")
+        print("2. Depositar.")
+        print("3. Mostrar saldo cajero.")
+        print("4. Salir.")
+
+        opcion = int(input("\nDigite un número del (1-4)\n"))
+                
+        if opcion == 1:
+            retiro = int(input("\nDigite una cantidad a retirar: \n"))
+            cajero.retirar(retiro)
+
+        elif opcion == 2:
+            deposito = int(input("\nDigite una cantidad a depositar: \n"))
+            cajero.depositar(deposito)
+        
+        elif opcion == 3:
+            cajero.mostrar_saldo()
+        elif opcion == 4:
+            print("\nGracias por usar el programa, Saliendo...\n")
+            break
+        else:
+            print("\nIngrese una opción correcta\n") 
+
+    except ValueError:
+        print("Error: Opción incorrecta")
+

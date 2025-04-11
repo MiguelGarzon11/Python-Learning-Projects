fbc3e728836a11d41a200ee9fca40f41ea9d0854
Miguel Angel <miguelangelgarb@gmail.com>
2025-04-10 19:09:52 -0500

    Rename

--- Object Oriented Programming/DunderMethods.py
+++ Object Oriented Programming/DunderMethods.py
@@ -1,49 +1,52 @@
-# 1. Inicialización y Representación. 
+# Dunder Methods Ejemplo practico 
 
-# __init__	Constructor (se ejecuta al crear el objeto)
-# __del__	Destructor (se ejecuta al eliminar el objeto)
-# __str__	Representación legible (print(obj))
-# __repr__	Representación técnica (repr(obj))
-# __format__	Personaliza format(obj)
-# __bytes__	Devuelve bytes: bytes(obj)
+class TiendaVirtual:
 
-# 2. Operadores Aritméticos.
+    def __init__(self, nombre, prod):
+        self._nombre = nombre
+        self._prod = prod
 
-# __add__	+
-# __sub__	-
-# __mul__	*
-# __truediv__	/
-# __floordiv__	//
-# __mod__	%
-# __pow__	**
-# __neg__	-obj
-# __pos__	+obj
+    def __repr__(self):
+        return f"TiendaVirtual(nombre={self._nombre}, productos={self._prod})"
+    
+    def __add__(self, other):
+        nueva_lista = self._prod + other._prod 
+        nuevo_nombre = self._nombre + "_" + other._nombre
+        return TiendaVirtual(nuevo_nombre, nueva_lista)
 
-# 3. Comparación
+    def __radd__(self, other):
+        if isinstance(other, int):
+            nuevo_producto = f'Producto {other}'
+            self._prod.append(nuevo_producto)
+        return self
+    
+    def __eq__(self, other):
+        if isinstance(other, TiendaVirtual):2
+        return self._nombre == other._nombre
+        return False
+    
+    def __lt__(self, other):
+        if isinstance(other, TiendaVirtual):
+            return len(self._prod) < len(other._prod)
+        return NotImplemented
 
-# __eq__	==
-# __ne__	!=
-# __lt__	<
-# __le__	<=
-# __gt__	>
-# __ge__	>=
 
-# 4. Tamaño y Contenido
+t1 = TiendaVirtual("TechStore", ["Mouse", "Teclado"])
+t2 = TiendaVirtual("OfficeStore", ["Silla"])
 
-# __len__	Longitud: len(obj)
-# __getitem__	Acceder por índice: obj[i]
-# __setitem__	Asignar por índice: obj[i] = x
-# __delitem__	Eliminar por índice: del obj[i]
-# __contains__	Usado por in: x in obj
-# __iter__	Iterador: for x in obj
-# __next__	Siguiente elemento en iteración
-# __reversed__	reversed(obj)
+# __repr__
+print(t1)  # TiendaVirtual(nombre='TechStore', productos=['Mouse', 'Teclado'])
 
-# 5. Atributos y llamadas.
+# __add__
+t3 = t1 + t2
+print(t3)  # TiendaVirtual(nombre='TechStore_OfficeStore', productos=['Mouse', 'Teclado', 'Silla'])
 
-# __getattr__	Cuando accedes a un atributo que no existe
-# __getattribute__	Se ejecuta siempre al acceder a cualquier atributo
-# __setattr__	Al asignar atributos: obj.atrib = valor
-# __delattr__	Al eliminar atributos: del obj.atrib
-# __dir__	Personaliza dir(obj)
-# __call__	Permite llamar al objeto como función: obj()
+# __radd__
+5 + t1
+print(t1)  # Producto 5 debería agregarse
+
+# __eq__
+print(t1 == t2)  # False
+
+# __lt__
+print(t1 < t2)  # Depende de cuántos productos tenga cada tienda

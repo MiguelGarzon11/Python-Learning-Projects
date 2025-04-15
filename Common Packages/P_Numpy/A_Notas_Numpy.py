e5e3a8660b04d8bd5bae0531ef66a54d3c9ea5fb
Miguel Angel <miguelangelgarb@gmail.com>
2025-04-15 18:42:13 -0500

    Numpy

---
+++ Common Packages/P_Numpy/A_Notas_Numpy.py
@@ -0,0 +1,27 @@
+import numpy as np
+from rich import print
+
+notas = np.array([
+  [3.5, 4.0, 2.8],
+  [4.2, 3.9, 4.5],
+  [2.8, 3.1, 3.6],
+  [5.0, 4.8, 4.9]
+])
+
+promedio = notas.mean()
+
+print("Número de estudiantes:", notas.shape[0])
+print("Número de materias:", notas.shape[1])
+print(f"Nota promedio general: {promedio:.2f}")
+print("Nota más alta:", notas.max())
+print("Nota más baja:", notas.min())
+
+p_fila = notas.mean(axis=1)
+for i, nota in enumerate(p_fila, start=1):
+    print(f"[yellow]Estudiante[/yellow] {i} [yellow]con promedio[/yellow] {nota:.1f}")
+
+p_materia = notas.mean(axis=0)
+for i, nota in enumerate(p_materia, start=1):
+    print(f"[green]Promedio de calificación para la materia[/green]{i}{nota}")
+    
+

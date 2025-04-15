8662c4af054b8f8be70fee3c759b655377d1cbc7
Miguel Angel <miguelangelgarb@gmail.com>
2025-04-15 16:38:38 -0500

    Numpy

---
+++ Common Packages/P_Numpy/main.py
@@ -0,0 +1,17 @@
+# Entendimiento de Numpy.
+
+# Es la librería base para cálculos numéricos y científicos. Permite trabajar con arrays, 
+# operaciones matemáticas rápidas, y sirve como base para otras librerías como pandas, 
+# scikit-learn, matplotlib, etc.
+
+# ===========Lo que aprenderás con NumPy:=================
+
+# Qué es un array de NumPy y cómo crearlo.
+# Operaciones básicas con arrays (suma, resta, producto, etc.).
+# Indexado y slicing.
+# Funciones estadísticas (mean, sum, std, etc.).
+# Broadcasting.
+# Generación de datos aleatorios.
+
+
+

4b871fb7c52b898cc1545cb310731e16560d31f8
Miguel Angel <miguelangelgarb@gmail.com>
2025-04-17 16:59:34 -0500

    Numpy

--- Common Packages/P_Numpy/C_temperatura.py
+++ Common Packages/P_Numpy/C_temperatura.py
@@ -12,6 +12,8 @@ temperaturas = np.array([
     [15, 16, 17, 19, 21, 23, 25, 26, 27, 27, 25, 23, 21],  # Domingo
 ])
 
+# Temperaturas miércoles
+
 t_miercoles = temperaturas[2]
 print("\n[bold cyan]Temperaturas registradas el miércoles[/bold cyan]")
 for i, temperatura in enumerate(t_miercoles, start=6):
@@ -19,6 +21,8 @@ for i, temperatura in enumerate(t_miercoles, start=6):
         print(f"[yellow]Temperatura registrada a las[/yellow] {i}:00 am: {temperatura}°")
     else:
         print(f"[red]Temperatura registrada a las[/red] {i}:00 pm: {temperatura}°")
+
+# Temperaturas registradas a las 12 pm.
 
 t_12pm = temperaturas[:,6]
 datos = ""
@@ -32,4 +36,23 @@ print(Panel.fit(
     border_style="violet"
     ))
 
+# Temperatura maxima de toda la semana.
 
+t_max = temperaturas.max()
+i_max = temperaturas.argmax() # Retorna el índice donde se encuentra el valor máximo
+print(f"La temperatura máxima de toda la semana fue {t_max}°, índice: {i_max}\n")
+
+# Temperatura promedio del jueves.
+
+t_jueves = temperaturas[3,:] # Accedo a los datos de la fila del día jueves.
+t_prom = t_jueves.mean()
+print(f"La temperatura promedio del día jueves es: {t_prom:,.2f}\n")
+
+# Día y hora donde se registró la temperatura máxima.
+
+indice_max = temperaturas.argmax()
+
+dia = indice_max // 13
+hora = indice_max % 13
+
+print(f"La temperatura máxima fue {t_max}° y se registró en el día {dia + 1} a las {hora + 6} horas.\n")
